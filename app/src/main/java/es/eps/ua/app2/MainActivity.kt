4445d027b53d5057d3af9dd3487074d315998cbe
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-10-20 14:01:04 +0200

    Creating layout

--- app/src/main/java/es/eps/ua/app2/MainActivity.kt
+++ app/src/main/java/es/eps/ua/app2/MainActivity.kt
@@ -1,15 +1,26 @@
 package es.eps.ua.app2
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.TelephonyManager
+import android.widget.Toast
 import es.eps.ua.app2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding : ActivityMainBinding
+    lateinit var tm : TelephonyManager
 
+    @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+         tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        Toast.makeText(this,tm.simSerialNumber.toString(),Toast.LENGTH_LONG ).show()
+
     }
 }
